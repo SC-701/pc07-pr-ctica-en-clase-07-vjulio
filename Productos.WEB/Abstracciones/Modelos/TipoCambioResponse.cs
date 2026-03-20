fb391d56c9ddc7b7baad9de7909dc34a69152c95
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-03-19 21:31:23 -0600

    web 0.5

---
+++ Productos.WEB/Abstracciones/Modelos/TipoCambioResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+
+namespace Abstracciones.Modelos
+{
+    
+
+    public class TipoCambioResponse
+    {
+        public bool estado { get; set; }
+        public string mensaje { get; set; }
+        public List<Datos> datos { get; set; }
+    }
+
+    public class Datos
+    {
+        public string titulo { get; set; }
+        public List<Indicador> indicadores { get; set; }
+    }
+
+    public class Indicador
+    {
+        public string codigoIndicador { get; set; }
+        public string nombreIndicador { get; set; }
+        public List<Serie> series { get; set; }
+    }
+
+    public class Serie
+    {
+        public DateTime fecha { get; set; }
+        public decimal valorDatoPorPeriodo { get; set; }
+    }
+}
