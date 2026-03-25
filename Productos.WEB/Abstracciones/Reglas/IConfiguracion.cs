47bfd8ec264489da769435a7d7302af378a1a513
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-03-25 12:33:46 -0600

    web index

--- Productos.WEB/Abstracciones/Reglas/IConfiguracion.cs
+++ Productos.WEB/Abstracciones/Reglas/IConfiguracion.cs
@@ -8,6 +8,7 @@ namespace Abstracciones.Reglas
 {
     public interface IConfiguracion
     {
+        
         public string ObtenerMetodo(string seccion, string nombre);
         public string ObtenerValor(string llave);
     }
--- Productos.WEB/Web/Program.cs
+++ Productos.WEB/Web/Program.cs
@@ -1,7 +1,12 @@
+using Abstracciones.Interfaces.Reglas;
+using Abstracciones.Reglas;
+using Reglas;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IConfiguracion, Configuracion>();
 
 var app = builder.Build();
 
