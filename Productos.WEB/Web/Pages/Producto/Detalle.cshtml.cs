a2d40540265d68974540e6f3d8fe3ceab0121fd9
Víctor Julio Rodríguez Cerdas <46943476+vjulio@users.noreply.github.com>
2026-03-25 16:40:04 -0600

    web detalle

--- Productos.WEB/Web/Pages/Producto/Detalle.cshtml.cs
+++ Productos.WEB/Web/Pages/Producto/Detalle.cshtml.cs
@@ -6,30 +6,29 @@ using System.Text.Json;
 
 namespace Web.Pages.Producto
 {
-    
-    public class IndexModel : PageModel
+    public class DetalleModel : PageModel
     {
-        private readonly IConfiguracion _configuracion;
-        public IList<ProductoResponse> productos { get; set; } = default!;
 
-        public IndexModel(IConfiguracion configuracion)
+        private readonly IConfiguracion _configuracion;
+        public ProductoResponse producto { get; set; } = default!;
+
+        public DetalleModel(IConfiguracion configuracion)
         {
             _configuracion = configuracion;
         }
 
-        public async Task OnGet()
+        public async Task OnGet(Guid? id)
         {
-            string endpoint = _configuracion.ObtenerMetodo("ApiEndPoints", "ObtenerTodos");
+            string endpoint = _configuracion.ObtenerMetodo("ApiEndPoints", "ObtenerId");
             var cliente = new HttpClient();
-            var solicitud = new HttpRequestMessage(HttpMethod.Get, endpoint);
+            var solicitud = new HttpRequestMessage(HttpMethod.Get, string.Format(endpoint,id));
 
             var respuesta = await cliente.SendAsync(solicitud);
             respuesta.EnsureSuccessStatusCode();
             var resultado = await respuesta.Content.ReadAsStringAsync();
             var opciones = new JsonSerializerOptions
             { PropertyNameCaseInsensitive = true };
-            productos = JsonSerializer.Deserialize<List<ProductoResponse>>
-                (resultado, opciones);
+            producto = JsonSerializer.Deserialize<ProductoResponse>(resultado, opciones);
         }
     }
 }
